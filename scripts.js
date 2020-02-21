86f5152c1f880336ff20439e73123c1b2f214624
Pedro Henrique de Oliveira <56042296+Pedrooliver13@users.noreply.github.com>
2020-02-21 01:23:20 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,17 @@
 const modalOverlay = document.querySelector('.modal__overlay')
-const cards = document.querySelectorAll('.modal__close')
+const cards = document.querySelectorAll('.modal__close i')
+const firstModalClose = document.querySelector('#firstModalClose i')
 
-for(let card of cards){
-    card.addEventListener("click" ,function menuOverlay(){
+for (let card of cards) {
+    card.addEventListener("click", function menuOverlay() {
         modalOverlay.classList.toggle('active')
 
-        modalOverlay.classList.add('animation')
+        if(!modalOverlay.classList.contains('active')) {
+            firstModalClose.classList.add('hide')
+        }
+        if(modalOverlay.classList.contains('active')){
+            firstModalClose.classList.remove('hide')
+        }
     })
 }
+
